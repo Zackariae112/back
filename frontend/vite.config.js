c4a2bbab20368d99b760b8214068b32eafd4d658
Zack <zack@Zacks-MacBook-Pro.local>
2025-05-06 22:21:09 +0100

    added base: ./

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // ✅ Important for Spring Boot static serving
+  base: './', // ✅ Important for Spring Boot static serving
   plugins: [react()],
   server: {
     proxy: {
